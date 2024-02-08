f641efabc2a461f9e31f098f6f4443d3526541fb
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2024-02-09 00:30:47 +0100

    comment buttons from sidebar

--- resources/views/livewire/side-bar-settings.blade.php
+++ resources/views/livewire/side-bar-settings.blade.php
@@ -46,7 +46,7 @@
 
             <hr class="h-px my-6 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent">
             <div class="flex mt-2 mb-12">
-                <h6 class="mb-0 dark:text-white">Light / Dark</h6>
+                <h6 class="mb-0 dark:text-white">Dark Mode</h6>
                 <div class="block pl-0 ml-auto min-h-6">
                     @if ($user->setting('theme')=="dark")
                         <input wire:click="savetheme('light')" checked dark-toggle class="rounded-10 duration-250 ease-in-out after:rounded-circle after:shadow-2xl after:duration-250 checked:after:translate-x-5.3 h-5 relative float-left mt-1 ml-auto w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-blue-500/95 checked:bg-blue-500/95 checked:bg-none checked:bg-right" type="checkbox">
@@ -55,12 +55,18 @@
                     @endif
                 </div>
             </div>
+
+            
+
+            <!--
             <div class="w-full text-center">
                 <span></span>
                 <h6 class="mt-4 dark:text-white">Thank you for sharing!</h6>
                 <a href="https://twitter.com/intent/tweet?text=Check%20Argon%20Dashboard%202%20Pro%20Tailwind%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23tailwindCSS&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fargon-dashboard-pro-tailwind" class="inline-block px-5 py-2.5 mb-0 mr-2 text-sm font-bold text-center text-white align-middle transition-all ease-in border-0 rounded-lg shadow-md cursor-pointer hover:shadow-xs hover:-translate-y-px active:opacity-85 leading-normal tracking-tight-rem bg-150 bg-x-25 me-2 border-slate-700 bg-slate-700" target="_blank"> <i class="mr-1 fab fa-twitter" aria-hidden="true"></i> Tweet </a>
                 <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/argon-dashboard-pro-tailwind" class="inline-block px-5 py-2.5 mb-0 mr-2 text-sm font-bold text-center text-white align-middle transition-all ease-in border-0 rounded-lg shadow-md cursor-pointer hover:shadow-xs hover:-translate-y-px active:opacity-85 leading-normal tracking-tight-rem bg-150 bg-x-25 me-2 border-slate-700 bg-slate-700" target="_blank"> <i class="mr-1 fab fa-facebook-square" aria-hidden="true"></i> Share </a>
             </div>
+            -->
+
         </div>
     </div>
 </div>
