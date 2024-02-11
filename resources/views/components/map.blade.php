241b47829571cf872d720160344a4002757c3c5e
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2024-02-11 15:29:50 +0100

    fix leaflet map dissapearing on livewire update

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -1,4 +1,4 @@
-<div id="location-map" class="form-control block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal" style="height: 290px"></div>
+<div id="location-map" class="form-control block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal" style="height: 290px"  wire:ignore></div>
 <script>
     var L = window.L;
 
