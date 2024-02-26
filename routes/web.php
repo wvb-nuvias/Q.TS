61fec4d6c0e81fe0068df6f75e03fec87f24fb98
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2024-02-26 16:00:29 +0100

    login fixes

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,18 @@ use App\Livewire\Dashboards\Incidents as Dashboard_Incidents;
 use App\Livewire\Dashboards\Devices as Dashboard_Devices;
 use App\Livewire\Dashboards\Subscriptions as Dashboard_Subscriptions;
 use App\Livewire\Integrations\Integrations;
-use App\Livewire\UserManagement;
-use App\Livewire\UserProfile;
 use App\Livewire\Subscriptions\Subscriptions;
 use App\Livewire\Organizations\Organizations;
 use App\Livewire\Contacts\Contacts;
 use App\Livewire\Incidents\Incidents;
 use App\Livewire\Devices\Devices;
 use App\Livewire\Alerts\Alerts;
+use App\Livewire\UserProfile;
+use App\Livewire\UserManagement;
 use App\Livewire\RoleManagement;
 use App\Livewire\TypeManagement;
-use App\Livewire\TenantManagement;
 use App\Livewire\ProductManagement;
-use App\Livewire\Svg\WatchguardIcon;
+use App\Livewire\TenantManagement;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,18 +47,18 @@ Route::middleware([
     Route::get('dashboards/incidents', Dashboard_Incidents::class)->name('dashboard_incidents');
     Route::get('dashboards/subscriptions', Dashboard_Subscriptions::class)->name('dashboard_subscriptions');
 
-    Route::get('integrations', Integrations::class)->name('integrations');
-    Route::get('incidents', Incidents::class)->name('incidents');
-    Route::get('subscriptions', Subscriptions::class)->name('subscriptions');
     Route::get('organizations', Organizations::class)->name('organizations');
     Route::get('contacts', Contacts::class)->name('contacts');
+    Route::get('subscriptions', Subscriptions::class)->name('subscriptions');
+    Route::get('incidents', Incidents::class)->name('incidents');
     Route::get('devices', Devices::class)->name('devices');
     Route::get('alerts', Alerts::class)->name('alerts');
+    Route::get('integrations', Integrations::class)->name('integrations');
 
     Route::get('user-profile', UserProfile::class)->middleware('auth')->name('user-profile');
     Route::get('user-management', UserManagement::class)->middleware('auth')->name('user-management');
-    Route::get('type-management', TypeManagement::class)->middleware('auth')->name('type-management');
     Route::get('role-management', RoleManagement::class)->middleware('auth')->name('role-management');
+    Route::get('type-management', TypeManagement::class)->middleware('auth')->name('type-management');
     Route::get('product-management', ProductManagement::class)->middleware('auth')->name('product-management');
 
     Route::get('tenant-management', TenantManagement::class)->middleware('auth')->name('tenant-management');
