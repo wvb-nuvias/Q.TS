f06f93e99d85e9c284377968281e7aed533fc382
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2024-03-05 12:12:03 +0100

    fixing search field

--- resources/views/vendor/livewire-powergrid/components/frameworks/tailwind/header/search.blade.php
+++ resources/views/vendor/livewire-powergrid/components/frameworks/tailwind/header/search.blade.php
@@ -1,6 +1,6 @@
 @if (data_get($setUp, 'header.searchInput'))
     <div class="flex flex-row mt-2 md:mt-0 w-full rounded-full justify-start sm:justify-center md:justify-end">
-        <div class="group relative rounded-full w-9/12 float-end float-right h-10 text-sm block mt-1 placeholder-gray-500 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 shadow-sm">
+        <div class="group relative rounded-full w-9/12 float-end h-10 text-sm block mt-1 placeholder-gray-500 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-1">
                 <span class="p-1 focus:outline-none focus:shadow-outline">
                     <x-livewire-powergrid::icons.search
